f0a038690d4ba65f31d9918fe63a2bf95e3fee29
Sp0oqY <kastierm45@gmail.com>
2024-04-18 18:47:10 +0200

    implemeted a bit of the backend

---
+++ backend/app.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS  
+from os import environ
+
+app = Flask(__name__)
+CORS(app) #Enable CORS for all routes
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def json(self):
+        return {"id": self.id, "name": self.name, "email": self.email}
+
+db.create_all()
+
+# Create a test route
+@app.route('/test', methods=['GET'])
+def test():
+    return jsonify({"message": "Hello World"})
+
+# Create a user
+@app.route('/api/flask/users', methods=['POST'])
+def create_user():
+    try:
+        data = request.get_json()
+        new_user = User(name=data['name'], email=data['email'])
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify({
+            "id": new_user.id, 
+            "name": new_user.name, 
+            "email": new_user.email
+        }), 201
+
+    except Exception as e:
+        return make_response(jsonify({"message": "error creating user", "error": str(e)}), 500)
+
+# Get all users
+@app.route('/api/flask/users', methods=['GET'])
+def get_users():
+    try:
+        users = User.query.all()
+        users_data = [{"id": user.id, "name": user.name, "email": user.email} for user in users]
+        return jsonify(users_data), 200
+    except Exception as e:
+        return make_response(jsonify({"message": "error getting users", "error": str(e)}), 500)
+
+# Get a user by id
+@app.route('/api/flask/users/<id>', methods=['GET'])
+def get_user(id):
+    try:
+        user = User.query.filter_by(id=id).first() # Get the first user with the id
+        if user:
+            return make_response(jsonify({"user": user.json()}), 200)
+        return make_response(jsonify({"message": "user not found"}), 404)
+        
+    except Exception as e:
+        return make_response(jsonify({"message": "error getting user", "error": str(e)}), 500)
